fa74b12391da9683f1ab7066e0567ee6b860014e
jendama <mabaifums@gmail.com>
2021-08-07 07:13:11 +0700

    refactoring project add .gitignore delete unused .gradle folder

--- app/src/main/java/com/fostiums/seaapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/fostiums/seaapp/ui/home/HomeFragment.kt
@@ -24,8 +24,8 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+
+
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -38,10 +38,8 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
+
+
 
         val fakeDataPromotion = arrayListOf(
             R.drawable.promosi_contoh,
